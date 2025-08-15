a8fc8fd833e5a075ee9aedf2bf6798c5ce5f9e0a
13720032621 <13720032621@139.com>
2025-08-15 16:20:38 +0800

    add pic

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,4 +1,4 @@
-# Configuration file for the Sphinx documentation builder.
+﻿# Configuration file for the Sphinx documentation builder.
 
 # -- Project information
 
@@ -16,20 +16,39 @@ extensions = [
     'sphinx.ext.doctest',
     'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
+    'sphinx.ext.githubpages',
     'sphinx.ext.intersphinx',
+	'myst_parser', 
+	'sphinxcontrib.mermaid', 
+    'sphinx-jsonschema'
 ]
 
-intersphinx_mapping = {
-    'python': ('https://docs.python.org/3/', None),
-    'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
-}
-intersphinx_disabled_domains = ['std']
-
 templates_path = ['_templates']
+exclude_patterns = ['build', 'Thumbs.db', '.DS_Store']
 
-# -- Options for HTML output
+language = 'zh_CN'
 
 html_theme = 'sphinx_rtd_theme'
+html_logo = "../static/ic_launcher_sphinx.png"
+#html_logo = "_static/ic_launcher_blues.png"
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+html_static_path = ['../static']
+html_show_sphinx = False
+html_show_sourcelink = False
+html_last_updated_fmt = '%Y年%m月%d日'
 
-# -- Options for EPUB output
-epub_show_urls = 'footnote'
+# 在 conf.py 末尾添加
+# latex_engine = 'xelatex'
+#
+# latex_elements = {
+#     'papersize': 'a4paper',
+#     'pointsize': '11pt',
+#     'fontpkg': r'''
+# \setmainfont{SimSun}
+# \setsansfont{SimHei}
+# \setmonofont{Fira Mono}
+# ''',
+#     'babel': r'\usepackage[UTF8]{ctex}',
+# }
